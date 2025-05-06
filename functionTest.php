e47795e399997bfaad690ebaac1ffdc96bc60550
math.arantes <matheusoficial1411@gmail.com>
2025-05-06 15:27:34 -0300

    curso 24%

--- functionTest.php
+++ functionTest.php
@@ -21,5 +21,22 @@
         echo "\n----------\nOlá\n----------\n";
     }
 
+    //A função sort($X); serve para ordenar de forma crescente o array X
+    $decimais = [9, 1, 2, 3, 4, 5, 6, 7, 8, 0];
+    sort($decimais);
+    var_dump($decimais);
+
+    //A função min($X) e max($X) servem para retornar o menor e o maior valor do array X respectivamente
+    $menorDecimal = min($decimais);
+    $maiorDecimal = max($decimais);
+    echo "\nO menor decimal é $menorDecimal e o maior é $maiorDecimal\n";
+
+    //Utilizar &$X torna o parâmetro X obrigatório de referência (por exemplo $dois em vez de 2)
+    function dobrarValor(int &$numero) : int{
+        return $numero * 2;
+    }
+    $dois = 2;
+    echo "\n----------------------\n" . dobrarValor($dois) . "\n----------------------\n";
+
     cumprimentar();
     echo "Sua soma é: " . somar(10, 10);
